f08f77ee9285456db85f73a5f870c24954054f7b
StreetShiffter <agwzdushaz@gmail.com>
2025-07-15 22:53:47 +0700

    Fix templates links

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'), # главная страницая
-    path('contacts/', views.feedback, name="{%url'catalog: contacts/'%}"),  # корректное имя
+    path('', views.home, name="home"), # главная страницая
+    path('contacts/', views.feedback, name="contacts"),  # корректное имя
 ]
