e8f1c5a6e8403cc26b9b589fd377d250a1374f26
StreetShiffter <agwzdushaz@gmail.com>
2025-08-06 22:28:21 +0700

    Create template app

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
-# from django.urls import path
-# from blog.apps import BlogConfig
-#
-# app_name = BlogConfig.name
-#
-# urlpatterns = [
-#     path('product/<int:pk>/', ProductDetailView.as_view(), name="product"),
-#     path('contacts/',FeedbackView.as_view(), name="contacts"),
-#     path('', HomeListView.as_view(), name="home"),
-# ]
+# blog/urls.py
+from django.urls import path
+from .views import BlogCreateView, BlogDetailView, BlogListView, BlogUpdateView, BlogDeleteView
+from .apps import BlogConfig
+
+app_name = BlogConfig.name  # или просто 'blog'
+
+urlpatterns = [
+    path('', BlogListView.as_view(), name='list'),           # /blog/
+    path('create/', BlogCreateView.as_view(), name='create'), # /blog/create/
+    path('<int:pk>/', BlogDetailView.as_view(), name='detail'),  # /blog/1/
+    path('<int:pk>/edit/', BlogUpdateView.as_view(), name='update'),  # /blog/1/edit/
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='delete'),  # /blog/1/delete/
+]
--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from django.views import View
+from django.urls import reverse_lazy
+
+from blog.models import BlogPost
+
 
 class BlogCreateView(CreateView):
-    pass
+    model = BlogPost
+    fields = '__all__'
+    template_name = 'blog/blogpost_form.html'
+    success_url = reverse_lazy('blog:list')
+
 
 class BlogUpdateView(UpdateView):
-    pass
+    model = BlogPost
+    template_name = 'blog/blogpost_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+    success_url = reverse_lazy('blog:list')
+
 
 class BlogDetailView(DetailView):
-    pass
+    model = BlogPost
+    template_name = 'blog/blogpost_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        bl_obj = super().get_object(queryset)
+        bl_obj.views_count += 1
+        bl_obj.save(update_fields=['views_count'])
+        return bl_obj
+
 
 class BlogListView(ListView):
-    pass
+    model = BlogPost
+    template_name = 'blog/blogpost_list.html'
+    context_object_name = 'posts'
+    paginate_by = 5  # Опционально: пагинация
+
+    def get_queryset(self):
+        # Только опубликованные посты
+        return BlogPost.objects.filter(is_published=True)
+
 
 class BlogDeleteView(DeleteView):
-    pass
+    model = BlogPost
+    template_name = 'blog/blogpost_confirm_delete.html'
+    success_url = reverse_lazy('blog:list')
